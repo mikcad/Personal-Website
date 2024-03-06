b394622ab98cabdfe514435c72aa79455264302a
mike <116024673+mikcad@users.noreply.github.com>
2024-03-06 18:41:10 +0000

    imported logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import './App.css'
+import './App.css';
+import logo from './assets/logo-img/mike-logo_487x105_no-bg.svg';
 
 function App() {
-  return <div>Hello</div>
+  return <div><img src={logo} alt="mike logo" height={50} width={100} /></div>;
 }
 
-export default App
+export default App;
