fe82ba53b4094d252869a47de6994d62dabf6aa5
mike <116024673+mikcad@users.noreply.github.com>
2024-03-07 13:11:25 +0000

    added routing
    - added react routing v6 to main.tsx
    - some small foundation to other pages
    -removed app.tsx (unused)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,6 @@
+import { Outlet } from 'react-router';
+import { NavLink } from 'react-router-dom';
+
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 import logo from '/logo-img/mike-logo_487x105_no-bg.svg';
@@ -5,29 +8,41 @@ import logo from '/logo-img/mike-logo_487x105_no-bg.svg';
 import './header.css';
 
 const Header = () => {
+  
   return (
-    <Navbar expand="lg" className="border-bottom p-4 justify-content-center">
-      <Container className="m-0">
-        <Navbar.Brand href="#home" className="px-4">
-          <img
-            src={logo}
-            alt="Mike Logo"
-            className="mike-logo"
-            height={40}
-            width={100}
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto column-gap-2 text-center">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand="lg" className="border-bottom p-4 justify-content-center">
+        <Container className="m-0">
+          <Navbar.Brand href="/" className="px-4">
+            <img
+              src={logo}
+              alt="Mike Logo"
+              className="mike-logo"
+              height={40}
+              width={100}
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto column-gap-2 text-center">
+              <Nav.Link>
+                <NavLink to={'/'}>Home</NavLink>
+              </Nav.Link>
+              <Nav.Link>
+                <NavLink to={'/about'}>About</NavLink>
+              </Nav.Link>
+              <Nav.Link>
+                <NavLink to={'/projects'}>Projects</NavLink>
+              </Nav.Link>
+              <Nav.Link>
+                <NavLink to={'/contact'}>Contact</NavLink>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import About from './pages/About/About.tsx';
+import Contact from './pages/Contact/Contact.tsx';
+import Home from './pages/Home/Home.tsx';
+import NotFound from './pages/ErrorPages/NotFound.tsx';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './global.css';
+import Header from './components/Header/Header.tsx';
+
+const router = createBrowserRouter([
+  {
+    element: <Header />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+    ],
+    errorElement: <NotFound />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
