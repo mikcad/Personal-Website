57f9d7d381222a91d2e708fd03ada1fe0c150b6a
mike <116024673+mikcad@users.noreply.github.com>
2024-03-07 11:07:39 +0000

    updated css
    - added new menu icon
    - edited some colors/nav css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,14 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
-import logo from '../../assets/logo-img/mike-logo_487x105_no-bg.svg';
+import logo from '/logo-img/mike-logo_487x105_no-bg.svg';
 
 import './header.css';
 
 const Header = () => {
   return (
-    <Navbar expand="lg" className="border-bottom p-4">
-      <Container>
-        <Navbar.Brand
-          href="#home"
-          className="border rounded-4 border-primary px-1"
-        >
+    <Navbar expand="lg" className="border-bottom p-4 justify-content-center">
+      <Container className="m-0">
+        <Navbar.Brand href="#home" className="px-4">
           <img
             src={logo}
             alt="Mike Logo"
@@ -22,7 +19,7 @@ const Header = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto gap-2">
+          <Nav className="ms-auto column-gap-2 text-center">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
