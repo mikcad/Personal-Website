e829eeaf9735f9fa4a39892e172a99aa0ddd0e8a
mike <116024673+mikcad@users.noreply.github.com>
2024-03-07 13:22:21 +0000

    smol commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import About from './pages/About/About.tsx';
+import Projects from './pages/Projects/Projects.tsx';
 import Contact from './pages/Contact/Contact.tsx';
 import Home from './pages/Home/Home.tsx';
 import NotFound from './pages/ErrorPages/NotFound.tsx';
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
       {
         path: '/about',
         element: <About />,
+      },
+      {
+        path: '/projects',
+        element: <Projects />,
       },
       {
         path: '/Contact',
