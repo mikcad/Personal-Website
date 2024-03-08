43aa465985e1a01ba5c5f672d384b9766cb4ccc3
mike <116024673+mikcad@users.noreply.github.com>
2024-03-08 18:08:59 +0000

    some formatting

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import Home from './pages/Home/Home.tsx';
 import About from './pages/About/About.tsx';
 import Projects from './pages/Projects/Projects.tsx';
 import Contact from './pages/Contact/Contact.tsx';
-import Home from './pages/Home/Home.tsx';
 import NotFound from './pages/ErrorPages/NotFound.tsx';
+
+import Header from './components/Header/Navbar.tsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './global.css';
-import Header from './components/Header/Navbar.tsx';
 
 const router = createBrowserRouter([
   {
