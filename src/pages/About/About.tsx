a25f2bb7c11b0fe11df12b797d2f1cf43b697857
mike <116024673+mikcad@users.noreply.github.com>
2024-03-08 18:13:59 +0000

    updated about
    - added basic wording and layout
    - added image/illustration

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,15 +1,28 @@
+import Card from 'react-bootstrap/Card';
+
+import './about.css';
+
 const About = () => {
   return (
-    <main id="about-content" className="container">
-      <header>
-         hello
-      </header>
-      <article>
-         about me :)
-      </article>
-      <section >
-      </section>
-    </main>
+    <section id='about-content'>
+      <div className="wrapper">
+        <h2>about</h2>
+        <h1>Who is Michael?</h1>
+        <div className="content-container d-flex justify-content-center align-items-center mt-4">
+          <div className="img-container">
+            <img src="/public/mike.jpg" alt="image illustration of michael" height={300} width={300} className='rounded-circle'/>
+          </div>
+          <div className="text-container">
+            <h4>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, est.
+              Ratione minus facilis, sint aut est voluptates exercitationem cum
+              temporibus quas porro, autem enim quisquam rerum quis quasi saepe
+              quae!
+            </h4>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
