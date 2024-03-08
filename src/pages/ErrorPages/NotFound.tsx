c8bf8ce35ec734b2f76d6eb06b7a877b23e0b1ff
mike <116024673+mikcad@users.noreply.github.com>
2024-03-08 18:10:13 +0000

    updated page not found

--- src/pages/ErrorPages/NotFound.tsx
+++ src/pages/ErrorPages/NotFound.tsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom"
 
 const NotFound = () => {
   return (
-    <div>404 Not Found</div>
+    <>
+
+    <section className="d-flex justify-content-center text-center mt-5">
+      <div className="wrapper">
+        <div>404 Not Found</div>
+        <Link to={"/"}>click me</Link>
+      </div>
+    </section>
+    </>
+
   )
 }
 
